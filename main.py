17f87a3e6cc41bfe7ce4153f51bae799961a60b2
VictorRavani <victorravani98@gmail.com>
2024-08-13 17:41:16 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, url_for, render_template
+
+#inicializacao
+app = Flask(__name__)
+
+#rotas 
+@app.route('/')
+def ola_mundo():
+    titulo = "Gestão de Usuários"
+    usuarios = [
+        {"nome": "Guilherme", "membro_ativo": True},
+        {"nome": "Julio", "membro_ativo": True},
+        {"nome": "Maria", "membro_ativo": False},
+    ]
+    return render_template ("index.html", titulo=titulo, usuarios=usuarios)
+
+@app.route('/sobre')
+def pagina_sobre():
+    return """
+        <b>ProgramadorPython </b>: assista os vídeos no
+        <a href="https://www.youtube.com/watch?v=fkXlSyWiXVg&list=PL39zbyHjgjrbsP3xFSc-YH-6FN8WNpglh&index=2"> Canal no Youtube </a>
+    """
+
+#execucao
+app.run(debug=True)
